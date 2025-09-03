4d0ac92ad8993880a6a3079d1095c587255462d8
y398 <181825694+y398@users.noreply.github.com>
2025-09-03 14:24:53 +0900

    画面遷移の作成

--- app/src/main/java/jp/ac/meijou/android/s241205180/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205180/MainActivity2.java
@@ -1,5 +1,6 @@
 package jp.ac.meijou.android.s241205180;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205180.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205180.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Activity3へ遷移するインテント
+        binding.buttonA.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity3.class);
+            startActivity(intent);
+        });
+
+        // ブラウザを起動するインテント
+        binding.buttonB.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(android.net.Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
         });
     }
 }
