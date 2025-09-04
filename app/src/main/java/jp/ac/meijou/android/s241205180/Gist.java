1fc38d409020888c65a5e0dabd34c24677e4dcdc
y398 <181825694+y398@users.noreply.github.com>
2025-09-04 11:59:05 +0900

    webapiからデータを取得し表示する機能追加

---
+++ app/src/main/java/jp/ac/meijou/android/s241205180/Gist.java
@@ -0,0 +1,11 @@
+package jp.ac.meijou.android.s241205180;
+
+import java.util.Map;
+
+public class Gist {
+    public Map<String, GistFile> files;
+
+    public static class GistFile {
+        public String content;
+    }
+}
